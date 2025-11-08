56cb20fff13751de3f903903dc12659bc790cd93
devadyuti <devadyuti@yahoo.com>
2025-11-08 11:30:43 +0000

    Add CDN pakcage distribution under services #2

--- services/distribution/index.js
+++ services/distribution/index.js
@@ -20,6 +20,46 @@ class DistributionService {
         return getSignedCookies(params);
     }
 
+    async getSignatureParameters(publicKeyId, privateKey, isPolicy = false, prefix = "", minutesToExpire = 30, minutesToGrant = 0, ipAddress = "0.0.0.0/0") {
+        const params = {};
+        const expiryDate = new Date(new Date().setMinutes(new Date().getMinutes() + minutesToExpire));
+        const grantDate = new Date(new Date().setMinutes(new Date().getMinutes() + minutesToGrant));
+        if (publicKeyId) {
+            params.keyPairId = publicKeyId;
+        }
+        if (privateKey) {
+            params.privateKey = privateKey;
+        }
+        if (isPolicy) {
+            params.policy = JSON.stringify({
+                Statement: [
+                    {
+                        Resource: prefix + '*',
+                        Condition: {
+                            DateLessThan: {
+                                "AWS:EpochTime": new Date(expiryDate).getTime() / 1000, // time in seconds
+                            },
+                            DateGreaterThan: {
+                                "AWS:EpochTime": new Date(grantDate).getTime() / 1000, // time in seconds
+                            },
+                            IpAddress: ipAddress
+                        },
+                    },
+                ],
+            });
+        } else {
+            if (minutesToExpire) {
+                params.dateLessThan = expiryDate;
+            }
+            if (minutesToGrant) {
+                params.dateGreaterThan = grantDate;
+            }
+            if (ipAddress) {
+                params.ipAddress = ipAddress;
+            }
+        }
+        return params;
+    }
 }
 
 // Export a singleton instance
@@ -28,6 +68,7 @@ export const getSignedUrlWithParameters = (params) => distributionService.getSig
 export const getSignedUrlWithPolicy = (params) => distributionService.getSignedUrlWithPolicy(params);
 export const getSignedCookiesWithParameters  = (params) => distributionService.getSignedCookiesWithParameters (params);
 export const getSignedCookiesWithPolicy = (params) => distributionService.getSignedCookiesWithPolicy(params);
+export const getSignatureParameters = (publicKeyId, privateKey, isPolicy = false, prefix = "", minutesToExpire = 30, minutesToGrant = 0, ipAddress = "0.0.0.0/0") => distributionService.getSignatureParameters(publicKeyId, privateKey, isPolicy, prefix, minutesToExpire, minutesToGrant, ipAddress);
 
 // Also export the class for backward compatibility
 export { DistributionService as Distribution };
